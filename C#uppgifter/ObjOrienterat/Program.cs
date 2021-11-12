2240a590ec7fa1dce5835f2e0c851a16101f5ecf
Pomopop <70264384+Pomopop@users.noreply.github.com>
2021-11-12 09:56:19 +0100

    ändrade addfoce till velocity

--- C#uppgifter/ObjOrienterat/Program.cs
+++ C#uppgifter/ObjOrienterat/Program.cs
@@ -6,10 +6,37 @@ namespace ObjOrienterat
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            Console.WriteLine(name);
 
+
+
+
+            int[] arr = {1,2,3,4,5};
+            int Måltal = 9;
+
+
+            arrayLoop()
         }
+
+
+        static int[] arrayLoop(int[] arr, int Måltal)
+        {
+            
+
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(i + j == Måltal)
+                    {
+                        return new int[] {i,j};
+
+                    }
+                }
+            }
+            return arr;
+         }
+
+
     }
 
 
