8904e693d9a6e466cf7bf3b23ef456b98aac5055
Pomopop <70264384+Pomopop@users.noreply.github.com>
2021-12-02 09:21:18 +0100

    dfddddf

---
+++ C#uppgifter/morgonuppgift1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arr = {1,2,3,4,5};
+           int[] svar ={};
+
+           int målsumma = 9;
+
+           foreach(int item in svar){
+               Console.WriteLine(svar);
+           }
+        }
+
+
+
+
+        static int Function(int[] arr, int Målsumma)
+        {
+            for(int i = 0; i <= 5; i++);
+            {
+                for(int j = 0; j <= 5; j++)
+                {
+                    if(i += j = Målsumma){
+                        
+                        svar = Svar.Append(i,j).ToArray;
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+}
